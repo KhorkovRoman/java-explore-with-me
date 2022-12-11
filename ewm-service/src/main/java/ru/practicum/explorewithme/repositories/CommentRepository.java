a19d523e419119e54804bbf5dfd0dbe58ed66a56
KhorkovRoman <exterior@yandex.ru>
2022-12-11 20:10:14 +0300

    feat: "@Repository back"

--- ewm-service/src/main/java/ru/practicum/explorewithme/repositories/CommentRepository.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/repositories/CommentRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.explorewithme.models.comment.Comment;
 import ru.practicum.explorewithme.models.user.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c " +
